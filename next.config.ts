0a0bfb05c0773c07392b7bf857d528ce798cf3f7
Abdul8446 <farhanfarhan832@gmail.com>
2025-07-11 18:42:05 +0530

    image links added

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['images.pexels.com','drive.google.com','images.unsplash.com','res.cloudinary.com','m.media-amazon.com','i.pinimg.com','img3.exportersindia.com','lifeline-foundation.org'],
+  },
 };
 
 export default nextConfig;
