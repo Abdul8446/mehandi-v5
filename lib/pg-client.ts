647ceb7762589a5ecd376adcda1a0907a2738854
Abdul8446 <farhanfarhan832@gmail.com>
2025-07-11 22:47:30 +0530

    some changes in the pg-client.ts

--- lib/pg-client.ts
+++ lib/pg-client.ts
@@ -1,15 +1,15 @@
 import { StandardCheckoutClient, Env } from "pg-sdk-node";
 import { randomUUID } from "crypto";
 
-const clientId = process.env.PHONEPE_CLIENT_ID;
-const clientSecret = process.env.PHONEPE_CLIENT_SECRET;
-const clientVersion = 1;
+const clientId = process.env.PHONEPE_CLIENT_ID as string;
+const clientSecret = process.env.PHONEPE_CLIENT_SECRET as string;
+const clientVersion = 1 as number;
 const env = Env.PRODUCTION; // Change to Env.PRODUCTION when going live
 
 export const getClient = () => {
   return StandardCheckoutClient.getInstance(
-    clientId!,
-    clientSecret!,
+    clientId,
+    clientSecret,
     clientVersion,
     env
   );
